7dc65ee06d8377ec25b7fca95d51b08387e7e88a
Vimal Parthan <vimalparthan@gmail.com>
2017-07-31 11:30:26 +0530

    HelloWorld

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,18 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		
+		int myFirstNumber = (10 + 5) + (2 * 10);
+		int mySecondNumber = 12;
+		int myThirdNumber = myFirstNumber *2;
+		
+		int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+		
+		int myLastOne = 1000 - myTotal;
+		
+		System.out.println(myLastOne);
 
 	}
 
