4699375a7afbe643f536e6f872ff29945d791089
Vimal Parthan <vimalparthan@gmail.com>
2017-08-01 21:20:24 +0530

    inheritance

---
+++ Inheritance/src/com/vimal/Main.java
@@ -0,0 +1,16 @@
+package com.vimal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal animal = new Animal("Animal", 1,1,5,5);
+	
+		Dog dog = new Dog("Yorkie",8,20,2,4,1,20,"long silky");
+//		dog.eat();
+		dog.move(3);
+//		dog.run();
+//		dog.move(5);
+	}
+
+}
